e017f02fc8fe26f68eb9b311b0b4f0c7cc4b3f8f
GannaKachalaba <111142980+GannaKachalaba@users.noreply.github.com>
2023-06-11 15:35:39 +0300

    fix add

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ List of All Contacts -  https://prnt.sc/ZNXSffA7lMEz;
 
 Get Contact by id - https://prnt.sc/WuEurlDLsAYP;
 
-Add Contact -  https://prnt.sc/UQGHW9P-XxpL;
+Add Contact -  https://prnt.sc/EusFIJjOTB4h;
 
 Remove Contact - https://prnt.sc/FoVsHQkoqfB3;
--- contacts.js
+++ contacts.js
@@ -1,9 +1,11 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
 
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 
-let lastContactId = 0;
+// let lastContactId = 0;
 
 // TODO: задокументувати кожну функцію
 async function listContacts() {
@@ -32,7 +34,7 @@ async function removeContact(contactId) {
 
 async function addContact(name, email, phone) {
   const contacts = await listContacts();
-  const newContact = { id: ++lastContactId, name, email, phone };
+  const newContact = { id: uuidv4(), name, email, phone };
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts));
   return newContact;
